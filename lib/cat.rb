463f2254bcee00649c3f9bae481bbafea4ee143d
Andrew Santos <asantos@alumni.nd.edu>
2019-07-18 14:09:56 -0400

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,22 @@
 class Cat
-  # code goes here
+  
+  attr_reader :name, :species
+  attr_accessor :mood, :owner
+
+  @@all = []
+  def initialize (name, owner)
+    @name = name
+    @species = "cat"
+    @owner = owner
+    @mood = "nervous"
+    Cat.all << self
+    owner.cats << self
+  end
+
+
+  def self.all
+    @@all
+  end
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,69 @@
 class Owner
-  # code goes here
+  
+  attr_reader :name, :species
+  attr_accessor :dogs, :cats
+
+  @@all = []
+  def initialize (name, species = "human")
+    @name = name
+    @species = species
+    @cats = []
+    @dogs = []
+    Owner.all << self
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    Owner.all.length
+  end
+
+  def self.reset_all
+    Owner.all.clear
+  end
+
+  def buy_dog (dog)
+    Dog.new(dog, self)
+  end
+
+  def buy_cat (cat)
+    Cat.new(cat, self)
+  end
+
+  def change_all_happiness(pets, mood)
+    pets.map{|pets| pets.mood = mood}
+  end
+
+
+  def walk_dogs
+    change_all_happiness(dogs, "happy")
+  end
+
+  def feed_cats
+    change_all_happiness(cats, "happy")
+  end
+
+  def sell_pets
+    change_all_happiness(dogs, "nervous")
+    change_all_happiness(cats, "nervous")
+    dogs.map{|dog| dog.owner = nil}
+    cats.map{|cat| cat.owner = nil}
+    dogs.clear
+    cats.clear
+  end
+
+  def list_pets
+    "I have #{dogs.length} dog(s), and #{cats.length} cat(s)."
+  end
+
+
+
+
+
 end
